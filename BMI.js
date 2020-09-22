31a7e63f60549b10722cdd623c9cc47d83c1aed7
J Akkermans <99044668@mydavinci.nl>
2020-09-22 02:09:05 +0200

    tweak

--- BMI.js
+++ BMI.js
@@ -8,17 +8,16 @@ function getValue(){
     var bmi = weight / (length * length);
 
     //Make it only show decimals
-    var result = bmi*10000;
-    result = Math.floor(result * 100) / 100;
-    document.getElementById("Result").innerHTML = "This is your bmi" + " " + result.toFixed(2);
+    bmi = Math.floor(result * 100) / 100;
+    document.getElementById("Result").innerHTML = "This is your bmi" + " " + bmi.toFixed(2);
 
     //Show underweight, normal, and overweight.
-    if(result < 18){
+    if(bmi < 18){
         document.getElementById("summary").innerHTML = "You are underweight.";
 
 
     }
-    else if(result > 25){
+    else if(bmi > 25){
         document.getElementById("summary").innerHTML = "You are overweight.";
     }
     else{
